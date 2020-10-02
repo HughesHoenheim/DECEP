895353332114e7b96445ef49e0050088609092bd
Josue Mejias <josueldinhoo@gmail.com>
2020-10-01 21:20:01 -0400

    header
    cambios minimos

--- index.php
+++ index.php
@@ -59,11 +59,13 @@
                                                     <option value="">CDA</option>
                                                     <option value="">USD</option>
                                                 </select>
+<!--                                                <img src="../DECEP/DECEP_IMG/imagenes%20UPR%20DECEP%20EN%20LINEA2.png" alt="">-->
                                             </div>
                                         </form>
                                     </div>
-                                    <ul class="contact-now">     
+                                    <ul class="contact-now">   
                                         <li>+777 2345 7886</li>
+<!--                                        <img src="../DECEP/DECEP_IMG/imagenes%20UPR%20DECEP%20EN%20LINEA.png" alt="">-->
                                     </ul>
                                 </div>
                                 <div class="header-info-right">
@@ -165,18 +167,20 @@
         <div class="slider-area ">
             <!-- Mobile Menu -->
             <div class="slider-active">
-                <div class="single-slider slider-height" data-background="assets/img/hero/h1_hero.jpg">
+                    <div class="single-slider slider-height" data-background="../DECEP/DECEP_IMG/image.jpg">
+<!--                <div class="single-slider slider-height" data-background="assets/img/hero/h1_hero.jpg">-->
                     <div class="container">
                         <div class="row d-flex align-items-center justify-content-between">
                             <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 d-none d-md-block">
                                 <div class="hero__img" data-animation="bounceIn" data-delay=".4s">
-                                    <img src="assets/img/hero/hero_man.png" alt="">
+<!--                                    <img src="assets/img/hero/hero_man.png" alt="">-->
+                                        <img src="../DECEP/DECEP_IMG/lobo-grande.png" alt="">
                                 </div>
                             </div>
                             <div class="col-xl-5 col-lg-5 col-md-5 col-sm-8">
                                 <div class="hero__caption">
                                     <span data-animation="fadeInRight" data-delay=".4s">60% Discount</span>
-                                    <h1 data-animation="fadeInRight" data-delay=".6s">Winter <br> Collection</h1>
+                                    <h1 data-animation="fadeInRight" data-delay=".6s">DECEP <br> UPRA</h1>
                                     <p data-animation="fadeInRight" data-delay=".8s">Best Cloth Collection By 2020!</p>
                                     <!-- Hero-btn -->
                                     <div class="hero__btn" data-animation="fadeInRight" data-delay="1s">

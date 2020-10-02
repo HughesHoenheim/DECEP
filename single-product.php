217a0400e724f35af8095587a34d3ccfbead529b
emma1855 <60629375+emma1855@users.noreply.github.com>
2020-10-02 15:45:46 -0700

    Single product

--- single-product.php
+++ single-product.php
@@ -12,7 +12,7 @@
                 <div class="row">
                     <div class="col-xl-12">
                         <div class="hero-cap text-center">
-                            <h2>product Details</h2>
+                            <h2>Curso</h2>
                         </div>
                     </div>
                 </div>
@@ -25,35 +25,34 @@
   <div class="product_image_area">
     <div class="container">
       <div class="row justify-content-center">
-        <div class="col-lg-12">
+        <div class="col-lg-8">
           <div class="product_img_slide owl-carousel">
             <div class="single_product_img">
-              <img src="assets/img/product/single_product.png" alt="#" class="img-fluid">
+              <img src="../DECEP/DECEP_IMG/Cursos_IMG/general_IMG/certiocupacionales/m6.jpg" alt="#" class="img-fluid">
             </div>
             <div class="single_product_img">
-              <img src="assets/img/product/single_product.png" alt="#" class="img-fluid">
+              <img src="../DECEP/DECEP_IMG/Cursos_IMG/general_IMG/certiocupacionales/m9.jpg" alt="#" class="img-fluid">
             </div>
             <div class="single_product_img">
-              <img src="assets/img/product/single_product.png" alt="#" class="img-fluid">
+              <img src="../DECEP/DECEP_IMG/Cursos_IMG/general_IMG/certiocupacionales/m8.jpg" alt="#" class="img-fluid">
             </div>
           </div>
         </div>
         <div class="col-lg-8">
           <div class="single_product_text text-center">
-            <h3>Foam filling cotton slow <br>
-                rebound pillows</h3>
+            <h3>Mec&aacute;nica de Reparaci&oacute;n y Mantenimiento de Motores Pequenos</h3>
             <p>
-                Seamlessly empower fully researched growth strategies and interoperable internal or “organic” sources. Credibly innovate granular internal or “organic” sources whereas high standards in web-readiness. Credibly innovate granular internal or organic sources whereas high standards in web-readiness. Energistically scale future-proof core competencies vis-a-vis impactful experiences. Dramatically synthesize integrated schemas. with optimal networks.
+                Los participantes se capacitar&aacute;n con las destrezas y conocimientos en la reparaci&oacute;n y mantenimiento de motores pequenos. Estudiar&aacute;n la combusti&oacute;n interna de motores de 2 y 4 ciclos o tiempos, sistemas electr&oacute;nicos, lubricaci&oacute;n y enfriamiento, entre otros. Una vez completado el curso podr&aacute;n reparar y dar mantenimiento a todo tipo de motor pequeno. Obtendr&aacute;n las destrezas para la b&uacute;squeda de empleo o auto-emplearse.
             </p>
             <div class="card_area">
                 <div class="product_count_area">
-                    <p>Quantity</p>
+                    <p>Cantidad</p>
                     <div class="product_count d-inline-block">
                         <span class="product_count_item inumber-decrement"> <i class="ti-minus"></i></span>
                         <input class="product_count_item input-number" type="text" value="1" min="0" max="10">
                         <span class="product_count_item number-increment"> <i class="ti-plus"></i></span>
                     </div>
-                    <p>$5</p>
+                    <p>250 horas de Contacto | Costo: $1,250.00</p>
                 </div>
               <div class="add_to_cart">
                   <a href="#" class="btn_3">add to cart</a>
@@ -70,6 +69,7 @@
       <div class="container">
           <div class="row justify-content-center">
               <div class="col-lg-8">
+<!--
                   <div class="subscribe_part_content">
                       <h2>Get promotions & updates!</h2>
                       <p>Seamlessly empower fully researched growth strategies and interoperable internal or “organic” sources credibly innovate granular internal .</p>
@@ -78,6 +78,7 @@
                           <a href="#" class="btn_1">Subscribe</a>
                       </div>
                   </div>
+-->
               </div>
           </div>
       </div>

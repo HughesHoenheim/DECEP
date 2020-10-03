dfa3043c468a7471b4e866ada2ecde9a3f81fd3e
emma1855 <60629375+emma1855@users.noreply.github.com>
2020-10-02 18:15:57 -0700

    checkout

--- checkout.php
+++ checkout.php
@@ -25,6 +25,7 @@
   <!--================Checkout Area =================-->
   <section class="checkout_area section_padding">
     <div class="container">
+<!--
       <div class="returning_customer">
         <div class="check_title">
           <h2>
@@ -52,26 +53,31 @@
             </button>
             <div class="creat_account">
               <input type="checkbox" id="f-option" name="selector" />
-              <label for="f-option">Remember me</label>
+              <label for="f-option">Recordar el nombre de usuario</label>
             </div>
-            <a class="lost_pass" href="#">Lost your password?</a>
+            <a class="lost_pass" href="#"> ¿Se te olvidó tu contraseña?</a>
           </div>
         </form>
       </div>
+-->
       <div class="cupon_area">
+<!--
         <div class="check_title">
           <h2>
             Have a coupon?
             <a href="#">Click here to enter your code</a>
           </h2>
         </div>
+-->
+<!--
         <input type="text" placeholder="Enter coupon code" />
         <a class="tp_btn" href="#">Apply Coupon</a>
+-->
       </div>
       <div class="billing_details">
         <div class="row">
           <div class="col-lg-8">
-            <h3>Billing Details</h3>
+            <h3>Detalles de Facturaci&oacute;n</h3>
             <form class="row contact_form" action="#" method="post" novalidate="novalidate">
               <div class="col-md-6 form-group p_star">
                 <input type="text" class="form-control" id="first" name="name" />
@@ -81,9 +87,11 @@
                 <input type="text" class="form-control" id="last" name="name" />
                 <span class="placeholder" data-placeholder="Last name"></span>
               </div>
+<!--
               <div class="col-md-12 form-group">
                 <input type="text" class="form-control" id="company" name="company" placeholder="Company name" />
               </div>
+-->
               <div class="col-md-6 form-group p_star">
                 <input type="text" class="form-control" id="number" name="number" />
                 <span class="placeholder" data-placeholder="Phone number"></span>
@@ -111,6 +119,7 @@
                 <input type="text" class="form-control" id="city" name="city" />
                 <span class="placeholder" data-placeholder="Town/City"></span>
               </div>
+<!--
               <div class="col-md-12 form-group p_star">
                 <select class="country_select">
                   <option value="1">District</option>
@@ -118,71 +127,85 @@
                   <option value="4">District</option>
                 </select>
               </div>
+-->
               <div class="col-md-12 form-group">
                 <input type="text" class="form-control" id="zip" name="zip" placeholder="Postcode/ZIP" />
               </div>
+<!--
               <div class="col-md-12 form-group">
                 <div class="creat_account">
                   <input type="checkbox" id="f-option2" name="selector" />
-                  <label for="f-option2">Create an account?</label>
+                  <label for="f-option2">¿Crear Cuenta?</label>
                 </div>
               </div>
+-->
               <div class="col-md-12 form-group">
+<!--
                 <div class="creat_account">
                   <h3>Shipping Details</h3>
                   <input type="checkbox" id="f-option3" name="selector" />
                   <label for="f-option3">Ship to a different address?</label>
                 </div>
+-->
+<!--
                 <textarea class="form-control" name="message" id="message" rows="1"
                   placeholder="Order Notes"></textarea>
+-->
               </div>
             </form>
           </div>
           <div class="col-lg-4">
             <div class="order_box">
-              <h2>Your Order</h2>
+              <h2>Tu Orden</h2>
               <ul class="list">
                 <li>
-                  <a href="#">Product
+                  <a href="#">Cursos
                     <span>Total</span>
                   </a>
                 </li>
                 <li>
-                  <a href="#">Fresh Blackberry
-                    <span class="middle">x 02</span>
-                    <span class="last">$720.00</span>
+                  <a href="#">Grooming
+                    <span class="middle">x 01</span>
+                    <span class="last">$175.00</span>
                   </a>
                 </li>
                 <li>
-                  <a href="#">Fresh Tomatoes
-                    <span class="middle">x 02</span>
-                    <span class="last">$720.00</span>
+                  <a href="#">Micresoft Excel
+                    <span class="middle">x 01</span>
+                    <span class="last">$175.00</span>
                   </a>
                 </li>
+<!--
                 <li>
                   <a href="#">Fresh Brocoli
                     <span class="middle">x 02</span>
                     <span class="last">$720.00</span>
                   </a>
                 </li>
+-->
               </ul>
               <ul class="list list_2">
+<!--
                 <li>
                   <a href="#">Subtotal
-                    <span>$2160.00</span>
+                    <span>$350.00</span>
                   </a>
                 </li>
+-->
+<!--
                 <li>
                   <a href="#">Shipping
                     <span>Flat rate: $50.00</span>
                   </a>
                 </li>
+-->
                 <li>
                   <a href="#">Total
-                    <span>$2210.00</span>
+                    <span>$350.00</span>
                   </a>
                 </li>
               </ul>
+<!--
               <div class="payment_item">
                 <div class="radion_btn">
                   <input type="radio" id="f-option5" name="selector" />
@@ -194,6 +217,7 @@
                   Store State / County, Store Postcode.
                 </p>
               </div>
+-->
               <div class="payment_item active">
                 <div class="radion_btn">
                   <input type="radio" id="f-option6" name="selector" />
@@ -201,17 +225,19 @@
                   <img src="img/product/single-product/card.jpg" alt="" />
                   <div class="check"></div>
                 </div>
+<!--
                 <p>
                   Please send a check to Store Name, Store Street, Store Town,
                   Store State / County, Store Postcode.
                 </p>
+-->
               </div>
               <div class="creat_account">
                 <input type="checkbox" id="f-option4" name="selector" />
-                <label for="f-option4">I’ve read and accept the </label>
-                <a href="#">terms & conditions*</a>
+                <label for="f-option4"> Al hacer el pedido, aceptas las Condiciones de Uso y el Aviso de Privacidad </label>
+<!--                <a href="#">terms & conditions*</a>-->
               </div>
-              <a class="btn_3" href="#">Proceed to Paypal</a>
+              <a class="btn_3" href="#">Procesar pago</a>
             </div>
           </div>
         </div>
